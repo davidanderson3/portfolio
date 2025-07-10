21e9f0c8891aae110c3c3d77e3c7228a03460362
Anderson, David (NIH/NLM) [E] <david.anderson@nih.gov>
2025-07-10 11:18:50 -0400

    server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 // Serve static files (like index.html, style.css, script.js)
 app.use(express.static(path.resolve(__dirname, '../')));
